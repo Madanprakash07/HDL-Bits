0ef365549e7eeb443f68d370573ce18177e0e211
Madan Prakash <159546407+Madanprakash07@users.noreply.github.com>
2024-12-21 15:25:08 +0530

    Update 3 Connecting ports by name .v

--- Modules Hierarchy/3 Connecting ports by name .v
+++ Modules Hierarchy/3 Connecting ports by name .v
@@ -6,5 +6,5 @@ module top_module (
     output out1,
     output out2
 );
-    mod_a M(out1, out2, a, b, c, d);
+    mod_a M(.out1(out1), .out2(out2), .in1(a), .in2(b), .in3(c), .in4(d));
 endmodule
